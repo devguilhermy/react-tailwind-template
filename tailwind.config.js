06692aaec6fdb969a6403ebbc70d6f5f5ff8b68f
Guilhermy <55157846+devguilhermy@users.noreply.github.com>
2022-09-09 13:20:52 -0300

    build: Migrates from CRA to Vite.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    content: ['./src/**/*.{tsx,ts,jsx,js}', './public/index.html'],
-    darkMode: 'class', // or 'media' or 'false'
-    theme: {
-        extend: {},
-    },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+	content: ['./src/**/*.{tsx,ts,jsx,js}', './index.html'],
+	darkMode: 'class', // or 'media' or 'false'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
 };
